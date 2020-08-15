4b98e88860d29fc253a0cc84b085795cddbc2066
Aaron Fienberg <atfienberg@psu.edu>
2020-08-15 16:44:30 -0400

    reduce uart baud rate; burst writes are working again

--- vivado_project/artyS7.runs/synth_1/top.tcl
+++ vivado_project/artyS7.runs/synth_1/top.tcl
@@ -17,7 +17,6 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param chipscope.maxJobs 2
 set_param xicom.use_bs_reader 1
 set_msg_config -id {HDL 9-1061} -limit 100000
 set_msg_config -id {HDL 9-1654} -limit 100000
