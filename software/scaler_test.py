07f62e6ab06ac4b4cc63d45f01e76f3fe71dedce
Aaron Fienberg <atfienberg@psu.edu>
2020-12-21 11:39:56 -0500

    set deadtime, pulser period in scaler_test

--- software/scaler_test.py
+++ software/scaler_test.py
@@ -20,11 +20,21 @@ SCALER_PERIOD_LOW = 0xBB9
 SCALER_CHAN_SEL = 0xBB8
 SCALER_CNT_HIGH = 0xBB7
 SCALER_CNT_LOW = 0xBB6
+DEADTIME_HIGH = 0xBB5
+DEADTIME_LOW = 0xBB4
+
+PULSER_PERIOD_HIGH = 0xBB3
+PULSER_PERIOD_LOW = 0xBB2
+PERIODIC_PULSER_ENABLE = 0xBB1
 
 CLK_FREQ = 125000000
 
 pulser_width = 2
 scaler_period = 1  # one second
+
+deadtime = 1  # microseconds
+
+pulser_period = 4  # ms
 
 
 def set_scaler_period(arty, period, clk_freq=CLK_FREQ):
@@ -33,6 +43,22 @@ def set_scaler_period(arty, period, clk_freq=CLK_FREQ):
 
     arty.fpga_write(SCALER_PERIOD_HIGH, period_in_cycles >> 16)
     arty.fpga_write(SCALER_PERIOD_LOW, period_in_cycles & 0xFFFF)
+
+
+def set_scaler_deadtime(arty, deadtime, clk_freq=CLK_FREQ):
+    """ deadtime shall be in microseconds """
+    deadtime_in_cycles = int(deadtime * clk_freq / 1e6)
+
+    arty.fpga_write(DEADTIME_HIGH, deadtime >> 16)
+    arty.fpga_write(DEADTIME_LOW, deadtime_in_cycles & 0xFFFF)
+
+
+def enable_periodic_pulser(arty, pulser_period, clk_freq=CLK_FREQ):
+    period_in_cycles = int(pulser_period * CLK_FREQ / 1e3)
+
+    arty.fpga_write(PULSER_PERIOD_HIGH, period_in_cycles >> 16)
+    arty.fpga_write(PULSER_PERIOD_LOW, period_in_cycles & 0xFFFF)
+    arty.fpga_write(PERIODIC_PULSER_ENABLE, 0x1)
 
 
 def read_scaler(arty, channel):
@@ -54,13 +80,15 @@ def main():
 
     # configure fpga regs
     arty.fpga_write(PULSER_WIDTH_REG, pulser_width)
-    arty.fpga_write(PULSER_IO_RESET, 0x3E)
+    arty.fpga_write(PULSER_IO_RESET, 0x3C)
     for i in range(2):
         arty.fpga_write(PULSER_TRIG_MASK[i], 0x0)
-    arty.fpga_write(DISCR_IO_RESET[0], 0xFFFE)
+    arty.fpga_write(DISCR_IO_RESET[0], 0xFFFC)
 
-    # set scaler period, wait two periods
+    # set scaler period, deadtime, enable periodic pulser, wait two periods
+    set_scaler_deadtime(arty, deadtime)
     set_scaler_period(arty, scaler_period)
+    enable_periodic_pulser(arty, pulser_period)
     print("Waiting...")
     time.sleep(2 * scaler_period)
     print_scaler_count(arty, [0, 1])
@@ -73,6 +101,9 @@ def main():
     for i in range(10):
         time.sleep(scaler_period / 5)
         print_scaler_count(arty, [0, 1])
+
+    # disable periodic pulser
+    arty.fpga_write(PERIODIC_PULSER_ENABLE, 0x0)
 
 
 if __name__ == "__main__":
